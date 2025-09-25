66df7c91edfa74a60d2f766d65823623325ba23c
shmelevik <yaroslav1992@gmail.com>
2025-09-25 20:39:28 +0300

    add "requires_verification" in auth/login

--- api/api_v1/auth.py
+++ api/api_v1/auth.py
@@ -11,7 +11,12 @@ from .users import router as users_router
 router = APIRouter(prefix=settings.api.v1.auth, tags=["Auth"])
 
 # /login /logout
-router.include_router(router=fastapi_users.get_auth_router(authentication_backend))
+router.include_router(
+    router=fastapi_users.get_auth_router(
+        authentication_backend,
+        requires_verification=True,
+    )
+)
 
 # /register
 router.include_router(router=fastapi_users.get_register_router(UserRead, UserCreate))
