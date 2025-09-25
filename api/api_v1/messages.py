b75ae9f43a19f20268fd5e9541edc3ef36e5c93f
shmelevik <yaroslav1992@gmail.com>
2025-09-25 23:21:08 +0300

    create views for messages

--- api/api_v1/messages.py
+++ api/api_v1/messages.py
@@ -1,9 +1,28 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
 
+
+from api.api_v1.fastapi_users_routers import current_user, current_super_user
 from core.config import settings
+from core.models.user import User
+from core.schemas.user import UserRead
 
 
 router = APIRouter(prefix=settings.api.v1.messages, tags=["Messages"])
 
 
-def get_user_messages(): ...
+@router.get("/")
+def get_user_messages(user: User = Depends(current_user)):
+
+    return {
+        "messages": ("m1", "m2", "m3"),
+        "user": UserRead.model_validate(user),
+    }
+
+
+@router.get("/secrets")
+def get_superuser_messages(user: Annotated[User, Depends(current_super_user)]):
+    return {
+        "messages": ("secret_m1", "secret_m2", "secret_m3"),
+        "user": UserRead.model_validate(user),
+    }
